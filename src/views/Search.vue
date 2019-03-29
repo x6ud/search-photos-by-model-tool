721cf6bb213769f16ac1f8d5a01ddbea2365b444
x6ud <x6ud@foxmail.com>
2019-03-29 16:23:08 +0800

    Mobile compatible

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,88 +1,96 @@
 <template>
     <div style="width: 100%; height: 100%;">
         <div class="wrapper">
-            <div class="column-1" style="width: 360px;">
+            <div class="column-1" :class="{collapsed: collapse}">
+                <div class="inner-wrapper">
+                    <div style="margin-bottom: 8px;">Species:</div>
 
-                <div style="margin-bottom: 8px;">Species:</div>
-
-                <div class="search" style="display: flex; margin-bottom: 8px;">
-                    <a-spin :spinning="loading"
-                            style="flex: 1 1; margin-right: 8px;">
-                        <a-select
-                            style="width: 100%;"
-                            placeholder="any"
-                            v-model="keyword">
-                            <a-select-option :key="''">any</a-select-option>
-                            <a-select-option v-for="tag in tags" :key="tag">{{tag}}</a-select-option>
-                        </a-select>
-                    </a-spin>
-                    <a-button type="primary" @click="search">Search</a-button>
-                </div>
-
-                <div style="margin-bottom: 8px;">Direction: (skull type dose not affect search results)</div>
-
-                <a-select v-model="model" style="width: 100%; margin-bottom: 8px;">
-                    <a-select-option v-for="option in models"
-                                     :key="option.path"
-                                     :value="option.path">
-                        {{option.name}}
-                    </a-select-option>
-                </a-select>
-
-                <model-viewer :width="360"
-                              :height="360"
-                              :model="model"
-                              :rotate-x.sync="rotateX"
-                              :rotate-y.sync="rotateY"
-                              :rotate-z.sync="rotateZ"
-                              :zoom.sync="zoom"
-                >
-                    <a style="position: absolute; right: 8px; top: 8px; line-height: 14px;"
-                       target="_blank" title="Author of this model"
-                       v-if="currentModelOrigin"
-                       :href="currentModelOrigin">
-                        <a-icon type="info-circle"/>
-                    </a>
-                </model-viewer>
-
-                <div class="slider-wrapper">
-                    <span class="prefix">X: {{rotateX}}; Y: {{rotateY}}; Z: </span>
-                    <a-slider class="slider" :included="false"
-                              v-model="rotateZ" :min="-180" :max="180"/>
-                    <div class="postfix">
-                        <span style="width: 2.5em; text-align: center; display: inline-block;">{{rotateZ}}</span>
-                        <a-button @click="rotateX = rotateY = rotateZ = 0" size="small">Reset</a-button>
+                    <div class="search" style="display: flex; margin-bottom: 8px;">
+                        <a-spin :spinning="loading"
+                                style="flex: 1 1; margin-right: 8px;">
+                            <a-select
+                                style="width: 100%;"
+                                placeholder="any"
+                                v-model="keyword">
+                                <a-select-option :key="''">any</a-select-option>
+                                <a-select-option v-for="tag in tags" :key="tag">{{tag}}</a-select-option>
+                            </a-select>
+                        </a-spin>
+                        <a-button type="primary" @click="search">Search</a-button>
                     </div>
-                </div>
 
-                <div class="info" style="color: #bfbfbf">
-                    <div>Author: x6udpngx</div>
-                    <div>
-                        Special Thanks: <a href="https://github.com/xrabohrok" target="_blank">xrabohrok</a>
-                        - Thank you for helping improve this tool!
-                    </div>
-                    <div>Latest update: 2019-03-28</div>
-                    <div>
-                        <a href="https://github.com/x6ud/x6ud.github.io/issues" target="_blank">Create an issue</a>
-                        <span> - Suitable photos are lacking in some directions, please leave a message if you find one on Flickr.</span>
-                    </div>
-                    <div>
-                        <a href="https://ko-fi.com/x6udpngx" target="_blank">
-                            <img src="../assets/ko-fi.png" alt="" width="14px" height="14px"
-                                 style="margin-right: .25em">
-                            <span style="vertical-align: middle;">Ko-fi.com/x6udpngx</span>
+                    <div style="margin-bottom: 8px;">Direction: (skull type dose not affect search results)</div>
+
+                    <a-select v-model="model" style="width: 100%; margin-bottom: 8px;">
+                        <a-select-option v-for="option in models"
+                                         :key="option.path"
+                                         :value="option.path">
+                            {{option.name}}
+                        </a-select-option>
+                    </a-select>
+
+                    <model-viewer :width="360"
+                                  :height="360"
+                                  :model="model"
+                                  :rotate-x.sync="rotateX"
+                                  :rotate-y.sync="rotateY"
+                                  :rotate-z.sync="rotateZ"
+                                  :zoom.sync="zoom"
+                                  class="model"
+                    >
+                        <a style="position: absolute; right: 8px; top: 8px; line-height: 14px;"
+                           target="_blank" title="Author of this model"
+                           v-if="currentModelOrigin"
+                           :href="currentModelOrigin">
+                            <a-icon type="info-circle"/>
                         </a>
+                    </model-viewer>
+
+                    <div class="slider-wrapper">
+                        <span class="prefix">X: {{rotateX}}; Y: {{rotateY}}; Z: </span>
+                        <a-slider class="slider" :included="false"
+                                  v-model="rotateZ" :min="-180" :max="180"/>
+                        <div class="postfix">
+                            <span style="width: 2.5em; text-align: center; display: inline-block;">{{rotateZ}}</span>
+                            <a-button @click="rotateX = rotateY = rotateZ = 0" size="small">Reset</a-button>
+                        </div>
                     </div>
+
+                    <div class="info" style="color: #bfbfbf">
+                        <div>Author: x6udpngx</div>
+                        <div>
+                            Special Thanks: <a href="https://github.com/xrabohrok" target="_blank">xrabohrok</a>
+                            - Thank you for helping improve this tool!
+                        </div>
+                        <div>Latest update: 2019-03-28</div>
+                        <div>
+                            <a href="https://github.com/x6ud/x6ud.github.io/issues" target="_blank">Create an issue</a>
+                            <span> - Suitable photos are lacking in some directions, please leave a message if you find one on Flickr.</span>
+                        </div>
+                        <div>
+                            <a href="https://ko-fi.com/x6udpngx" target="_blank">
+                                <img src="../assets/ko-fi.png" alt="" width="14px" height="14px"
+                                     style="margin-right: .25em">
+                                <span style="vertical-align: middle;">Ko-fi.com/x6udpngx</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="collapse-handler" @click="collapse = !collapse">
+                    <a-icon class="icon" type="up"/>
                 </div>
             </div>
 
             <div class="column-2">
-                <image-thumb class="item"
-                             v-for="item in result"
-                             :image="item"
-                             :key="item.url"
-                             :size="200"
-                             @click.native="show(item)"/>
+                <div class="inner-wrapper">
+                    <image-thumb class="item"
+                                 v-for="item in result"
+                                 :image="item"
+                                 :key="item.url"
+                                 :size="200"
+                                 @click.native="show(item)"/>
+                </div>
             </div>
         </div>
 
@@ -125,7 +133,9 @@
                 // whether to display fullscreen image
                 showLarge: false,
                 // is loading data
-                loading: false
+                loading: false,
+                // collapse search on mobile device
+                collapse: false
             };
         },
         computed: {
@@ -174,6 +184,8 @@
                 // first 30 best results
                 result.sort((a, b) => a.match - b.match);
                 this.result = result.slice(0, Math.min(result.length, 30));
+
+                this.collapse = true;
             },
             show(item) {
                 this.currentImage = item;
@@ -229,21 +241,108 @@
         .column-1 {
             flex: 0 0 360px;
             margin-right: 10px;
+            width: 360px;
+
+            .collapse-handler {
+                display: none;
+            }
         }
 
         .column-2 {
             flex: 1 1;
             height: 100%;
-            min-width: 360px;
-            box-sizing: border-box;
-            border: 1px solid #d9d9d9;
-            border-radius: 4px;
-            padding: 10px;
-            overflow-y: scroll;
 
-            .item {
-                margin: 5px;
-                cursor: zoom-in;
+            .inner-wrapper {
+                width: 100%;
+                height: 100%;
+                box-sizing: border-box;
+                border: 1px solid #d9d9d9;
+                border-radius: 4px;
+                padding: 10px;
+                overflow-y: scroll;
+
+                .item {
+                    margin: 5px;
+                    cursor: zoom-in;
+                }
+            }
+        }
+    }
+
+    @media screen and (max-height: 480px) {
+        .wrapper {
+            padding: 10px !important;
+        }
+    }
+
+    @media screen and (max-width: 480px) {
+        .wrapper {
+            display: block !important;
+            overflow: hidden;
+            position: relative;
+            box-sizing: border-box;
+            width: 100%;
+            height: 100%;
+            padding: 0 !important;
+
+            .column-1 {
+                position: fixed;
+                left: 0;
+                top: 0;
+                z-index: 1;
+                width: 100%;
+                height: 100%;
+                box-sizing: border-box;
+                background-color: #fff;
+                overflow: auto;
+                transition: height .3s;
+
+                .inner-wrapper {
+                    width: 360px;
+                    margin: 0 auto;
+                    padding: 10px 0;
+                }
+
+                $handler-size: 48px;
+
+                .collapse-handler {
+                    display: block !important;
+                    position: fixed;
+                    left: 50%;
+                    top: 100%;
+                    width: $handler-size;
+                    height: $handler-size;
+                    margin: (-$handler-size / 2) 0 0 (-$handler-size / 2);
+                    border-radius: 100%;
+                    overflow: hidden;
+                    background-color: rgba(0, 0, 0, 0.2);
+                    color: #fff;
+                    font-size: 20px;
+                    text-align: center;
+
+                    .icon {
+                        width: $handler-size;
+                    }
+                }
+
+                &.collapsed {
+                    height: 0 !important;
+
+                    .collapse-handler {
+                        top: 0 !important;
+
+                        .icon {
+                            transform-origin: 50% 50%;
+                            transform: rotate(180deg);
+                            margin-top: $handler-size / 2;
+                        }
+                    }
+                }
+            }
+
+            .column-2 {
+                box-sizing: border-box;
+                padding: 10px;
             }
         }
     }

d61be5583ffecab2d5d503465d7a250fe3f602fd
x6ud <x6ud@foxmail.com>
2019-06-12 09:04:17 +0800

    Fixed some urls not getting flicker id correctly

--- src/get-flickr-id.js
+++ src/get-flickr-id.js
@@ -1,8 +1,8 @@
 /**
- * Return picture id if it's a Flickr url, or else return null.
+ * Return picture id if it's a Flickr url, otherwise return null.
  */
 export default function (url) {
     url += '';
-    const match = url.match(/https:\/\/farm[0-9]+\.static\.flickr\.com\/[0-9]+\/([0-9]+)_/);
+    const match = url.match(/flickr\.com\/[0-9]+\/([0-9]+)_/);
     return match ? match[1] : null;
 };
